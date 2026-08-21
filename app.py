cbdb647f657f6d4da12efa0258386b88a2291e34
txmzyyy <tamaramk08@gmail.com>
2026-08-21 06:38:09 +0300

    complete user model and migrations

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from extensions import db, migrate, jwt, cors
+from config import Config
+from models import User
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-@app.route('/')
-def home():
-    return "Flask is running!"
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    cors.init_app(app)
+
+    return app
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
