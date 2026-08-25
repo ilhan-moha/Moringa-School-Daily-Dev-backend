608935f4933937a8acd5965bd4d82417c0b974a5
txmzyyy <tamaramk08@gmail.com>
2026-08-25 09:55:55 +0300

    feat:add signup and login routes

--- services/auth_service.py
+++ services/auth_service.py
@@ -2,6 +2,7 @@ from models.user import User
 from extensions import db
 
 def register_user(first_name, last_name, email, password):
+    
     existing_user = User.query.filter_by(email=email).first()
     if existing_user:
         raise ValueError("Email already registered")
