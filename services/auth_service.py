2da5ac1cb9fd8e74c1756113e94494bd9b7b216d
ilhan moha <mohamudilhan99@gmail.com>
2026-09-03 23:54:04 +0300

    add role user and writer when signing up

--- services/auth_service.py
+++ services/auth_service.py
@@ -6,6 +6,9 @@ def register_user(first_name, last_name, email, password):
     existing_user = User.query.filter_by(email=email).first()
     if existing_user:
         raise ValueError("Email already registered")
+
+    if role not in ("user", "writer"):
+     role = "user"
     user = User(
         first_name=first_name,
         last_name=last_name,
