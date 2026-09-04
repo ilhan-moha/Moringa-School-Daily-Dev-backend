57e940b9bc545c129794804d18e723a69cc725f4
ilhan moha <mohamudilhan99@gmail.com>
2026-09-04 14:31:36 +0300

    add new vercel

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def create_app():
 
     cors.init_app(
         app,
-        origins=["https://daily-dev-lime.vercel.app", "http://localhost:5173"],
+        origins=["https://daily-dev-lime.vercel.app","https://daily-dev-7rgw-nzwxxxv5v-ilhan4.vercel.app", "http://localhost:5173"],
         supports_credentials=True,
         allow_headers=["Content-Type", "Authorization"],
         methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
